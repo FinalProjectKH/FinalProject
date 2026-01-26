bbdca6542461a1c37fb84f5abe51c264da4ff605
P1GEN6-SOLBI\bodun_56xh2zw <boduni20@gmail.com>
2026-01-26 18:16:22 +0900

    0126

--- react-finalproject-japcompany/src/App.jsx
+++ react-finalproject-japcompany/src/App.jsx
@@ -3,22 +3,24 @@ import MyCalendar from "./pages/Calendar";
 import MainLayout from "./components/layout/MainLayout";
 import AttendanceLayout from "./components/layout/attendance/AttendanceLayout";
 import LoginPage from "./pages/LoginPage";
+import NotFound from './pages/NotFound';
+
 
 const App = () => {
   return (
     <>
-    {/* <LoginPage /> */}
-
-    {/* <MainLayout /> */}
-
-    {/* <AttendanceLayout /> */}
-    {/* <MyCalendar /> */}
 
     
       <Routes>
         <Route path="/" element={<MainLayout />} />
         <Route path = "/attendance" element={<AttendanceLayout/>} ></Route>
         <Route path = "/calendar" element={<MyCalendar/>} ></Route>
+
+
+
+
+        <Route path="*" element={<NotFound />}></Route> 
+        {/* 얘를 제일 마지막에 작성해서 없는 경로 전부 404처리 */}
       </Routes>
     
 
