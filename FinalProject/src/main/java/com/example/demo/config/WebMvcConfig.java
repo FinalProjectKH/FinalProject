f4da064a4662d14cf424949f5c294e758b081705
P1GEN6-SOLBI\bodun_56xh2zw <boduni20@gmail.com>
2026-02-23 12:31:43 +0900

    webconfig

--- FinalProject/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ FinalProject/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -1,12 +1,23 @@
 package com.example.demo.config; // 패키지명은 본인 프로젝트에 맞게 수정
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 API 주소에 대해
+                .allowedOriginPatterns("*") // 일단 모든 도메인 허용 (나중에 Vercel 주소로 변경 권장)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*")
+                .allowCredentials(true); // 쿠키나 인증 정보 포함 허용
+    }
+	
+	
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 🔥 핵심: 브라우저에서 "/uploads/**" 로 요청이 오면
